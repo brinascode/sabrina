6a93631e48cea69170c40f3e40e89add63348f53
Sabrina Koumoin <sak96528@marymount.edu>
2020-04-08 02:35:59 +0000

    same bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch,Route} from "react-router"
+import Banner from "./components/Banner"
+import AboutMe from "./pages/AboutMe.jsx"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch>
+         <Banner />
+         <Route exact path="/" component={AboutMe} />
+         <Route path="/" component={AboutMe} />
+     </Switch>
     </div>
   );
 }
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function AboutMe(){
+    return(
+        <div>
+            ABout me
+        </div>
+    )
+}
